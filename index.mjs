fd34c754cfcd95cdef85c9113f87e714f6fb7473
Darkness-cpu <mistakessadly@gmail.com>
2024-12-18 07:27:25 +0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -30,7 +30,7 @@ const youtube_parser = (url) => {
 };
 
 // Serve the dashboard
-app.get('/dashboard', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
